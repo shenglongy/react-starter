f1baa2886b7e5b60da2f1860328f8a693d3bd43a
shenglongy <shenglongy@opera.com>
2021-09-13 17:46:20 +0800

    feat: add tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const isProduction = process.env.NODE_ENV === "production";
 module.exports = {
-  plugins: [require("postcss-preset-env")],
+  plugins: [
+    require("postcss-preset-env"),
+    require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer"),
+    ...(isProduction
+      ? [
+          require("cssnano")({
+            preset: "default",
+          }),
+        ]
+      : []),
+  ],
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
+import "./assets/styles/tailwind.scss";
 import { store } from "./store";
 
 ReactDOM.render(
